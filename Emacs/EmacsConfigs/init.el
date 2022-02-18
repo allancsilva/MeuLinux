4c60f617c2a71c02575cf48fb0f17139f5dd3bd9
Allan Caetano <91976669+allancsilva@users.noreply.github.com>
2022-02-18 06:44:40 -0300

    att emacs

--- Emacs/EmacsConfigs/init.el
+++ Emacs/EmacsConfigs/init.el
@@ -11,7 +11,8 @@
 (global-display-line-numbers-mode t)
 (setq kill-do-not-save-duplicates t)
 (setq next-line-add-newlines nil)
-(setq initial-scratch-message nil)      
+(setq initial-scratch-message nil)   
+(setq auto-save-default t)   
 
 (setq scroll-step 1) 
 (setq use-dialog-box nil)
@@ -20,6 +21,7 @@
 (setq create-lockfiles nil)
 (defalias 'yes-or-no-p 'y-or-n-p)
 (setq-default tab-width 2)
+(setq enable-local-variables :all)
 
 ;; (setq mode-line-format nil)
 ;; (setq-default mode-line-format nil)
@@ -35,16 +37,14 @@
 (setq evil-want-keybinding nil)
 (setq show-paren-style 'expression)
 
-(setq w! "uUuuuuuUUUuuuummmMMMmmmm.  This ain't vim.")
-
-;; (set-face-font 'default "Hack 12")
 (setq default-frame-alist
       (append (list '(width  . 72) '(height . 40)
                     '(vertical-scroll-bars . nil)
-                    '(internal-border-width . 16)
-                    '(font . "Hack 12"))))
+                    '(internal-border-width . 18)
+                    '(font . "Fira Code 13")
+                    )))
 (set-frame-parameter (selected-frame)
-                     'internal-border-width 16)
+                     'internal-border-width 18)
 ;; Underline line at descent position, not baseline position
 (setq x-underline-at-descent-line t)
 
@@ -78,7 +78,7 @@
                                 (emacs-init-time)))))
 
 
-;; Aqui o Straigth el que funciona que nem o Package do Melpa
+;; Repositorios aqui 
 (defvar bootstrap-version)
 (let ((bootstrap-file
        (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
@@ -98,43 +98,25 @@
 ;; Load the helper package for commands like `straight-x-clean-unused-repos'
 (require 'straight-x)
 
-
-;; Aqui o Package com o Melpa normal
 (require 'package)
-(setq package-archives '(("melpa" . "https://melpa.org/packages/")
-                         ("org" . "https://orgmode.org/elpa/")))
-(setq use-package-always-ensure t)
+(package-initialize)
+(setq package-archives '( ("gnu" . "http://elpa.gnu.org/packages/")                 
+                          ("elpa" . "http://tromey.com/elpa/")
+                          ("melpa" . "http://melpa.org/packages/")
+                          ("org" . "https://orgmode.org/elpa/")))
 
+;; Install dependencies
 (when (not (package-installed-p 'use-package))
   (package-refresh-contents)
   (package-install 'use-package))
-
+(setq-default use-package-always-ensure t)
+  
 
 ;; Temas
-;; (use-package moe-theme
-;;   :init
-;;   (setq moe-theme-highlight-buffer-id t)
-;;   :config
-;;   (emagician/defhook update-moe-color midnight-hook
-;;     (moe-theme-set-color (emagician/get-moe-color-for-day))
-;;     (emagician/powerline-theme))
-;;   (moe-theme-set-color (emagician/get-moe-color-for-day))
-;;   (moe-dark)
-;;   (powerline-moe-theme)
-;;   (emagician/powerline-theme)
-;;   (powerline-reset)
-;; )
-
-;; (straight-use-package '(nano-theme :type git :host github
-;;                                    :repo "rougier/nano-theme"))
-;; (require 'nano-theme)
-;; (nano-mode) (nano-dark)
-
 (use-package chocolate-theme
    :ensure t
    :config
    (load-theme 'chocolate t))
-
 
 (define-key mode-line-major-mode-keymap [header-line]
   (lookup-key mode-line-major-mode-keymap [mode-line]))
@@ -146,7 +128,7 @@
      '((:eval
        (mode-line-render
        (format-mode-line (list
-         (propertize " ☰" 'face `(:inherit mode-line-buffer-id)
+         (propertize "   ☰" 'face `(:inherit mode-line-buffer-id)
                          'help-echo "Mode(s) menu"
                          'mouse-face 'mode-line-highlight
                          'local-map   mode-line-major-mode-keymap)
@@ -156,16 +138,29 @@
        (format-mode-line
         (propertize "%4l:%2c  " 'face `(:inherit face-faded)))))))
 
-
-;; Comment if you want to keep the modeline at the bottom
-;; (setq-default header-line-format mode-line-format)
-;; (setq-default mode-line-format'(
-;;  ""))
 ;; Vertical window divider
 (setq window-divider-default-right-width 3)
 (setq window-divider-default-places 'right-only)
 (window-divider-mode)
 
+;; vterm
+(use-package vterm
+  :config
+  (defun turn-off-chrome ()
+    (hl-line-mode -1)
+    (display-line-numbers-mode -1))
+  :hook (vterm-mode . turn-off-chrome))
+
+(use-package vterm-toggle
+  :custom
+  (vterm-toggle-fullscreen-p nil "Open a vterm in another window.")
+  (vterm-toggle-scope 'project)
+  :bind (("C-c t" . #'vterm-toggle)
+         :map vterm-mode-map
+         ("C-\\" . #'popper-cycle)
+         ("s-t" . #'vterm) ; Open up new tabs quickly
+         ("s-v" . #'vterm-yank)
+         ))
 
 ;; Let's Be Evil !!
 (straight-use-package 'evil)
@@ -179,22 +174,23 @@
 (straight-use-package 'evil-commentary)
 (evil-commentary-mode)
 
+;; Geral
 (straight-use-package 'which-key)
 (which-key-mode)
 (setq which-key-popup-type 'minibuffer)
 
-(use-package perspective
-  :demand t
-  :bind (("C-x C-b" . persp-list-buffers)
-         ("C-M-k" . persp-switch)
-         ("C-M-l" . persp-next)
-         ("C-x k" . persp-kill-buffer*))
-  :custom
-  (persp-initial-frame-name "Main")
-  :config
-  ;; Running `persp-mode' multiple times resets the perspective list...
-  (unless (equal persp-mode t)
-    (persp-mode)))
+;; (use-package perspective
+;;   :demand t
+;;   :bind (("C-x C-b" . persp-list-buffers)
+;;          ("C-M-k" . persp-switch)
+;;          ("C-M-l" . persp-next)
+;;          ("C-x k" . persp-kill-buffer*))
+;;   :custom
+;;   (persp-initial-frame-name "Main")
+;;   :config
+;;   ;; Running `persp-mode' multiple times resets the perspective list...
+;;   (unless (equal persp-mode t)
+;;     (persp-mode)))
 
 (use-package page-break-lines
   :ensure t
@@ -210,6 +206,10 @@
   :ensure t
   :if (display-graphic-p))
 
+(use-package all-the-icons-dired
+  :after all-the-icons
+  :hook (dired-mode . all-the-icons-dired-mode))
+
 (straight-use-package 'yasnippet)
 (straight-use-package 'yasnippet-snippets)
 (yas-global-mode t)
@@ -219,8 +219,9 @@
   :after (yasnippet)
   :config (yas-global-mode 1))
 
-(use-package show-marks
-  :ensure t)
+(use-package color-identifiers-mode
+  :init
+  (add-hook 'after-init-hook 'global-color-identifiers-mode))
 
 (use-package volatile-highlights
   :diminish ""
@@ -234,6 +235,7 @@
   (prog-mode . highlight-thing-mode))
 
 (use-package highlight-indent-guides
+	:if (display-graphic-p)
   :hook (prog-mode . highlight-indent-guides-mode)
   :init
   (setq highlight-indent-guides-method 'character)
@@ -243,7 +245,15 @@
 (use-package smartparens
   :ensure t
   :config
-    (add-hook 'prog-mode-hook 'smartparens-mode))
+  (add-hook 'prog-mode-hook 'smartparens-mode)
+	(setq
+	   sp-base-key-bindings 'paredit
+	   sp-autoskip-closing-pair 'always
+	   sp-hybrid-kill-entire-symbol nil
+     sp-show-pair-from-inside nil)
+  (smartparens-global-mode 1)
+  (require 'smartparens-config)
+  :diminish smartparens-mode)
 
 (use-package rainbow-delimiters
   :init
@@ -251,7 +261,9 @@
   (add-hook 'geiser-repl-mode-hook 'rainbow-delimiters-mode)
   (add-hook 'cider-repl-mode-hook 'rainbow-delimiters-mode))
 
-(use-package transpose-frame)
+(use-package ws-butler
+  :hook ((text-mode . ws-butler-mode)
+         (prog-mode . ws-butler-mode)))
 
 (use-package neotree
   :bind
@@ -263,13 +275,10 @@
   (setq neo-smart-open t)
   (setq neo-window-width 25)
   (setq-default neo-show-hidden-files t)
-
   (add-hook 'neo-after-create-hook
             (lambda (&rest _) (display-line-numbers-mode -1)))
-
   (add-hook 'neo-after-create-hook
             (lambda (&rest _) (setq mode-line-format nil)))
-
   (add-hook 'neo-after-create-hook
             (lambda (&rest _) (setq header-line-format nil))))
 
@@ -400,52 +409,99 @@
   (marginalia-mode))
 
 ;; Linguagens aqui
+;; (use-package flycheck
+;;   :init
+;;   (global-flycheck-mode))
 
 (use-package company
-  :init (add-hook 'after-init-hook 'global-company-mode)
+  :ensure t
+  :delight
+  :bind (("C-c ." . company-complete)
+         ("C-c C-." . company-complete)
+         ("C-c s s" . company-yasnippet)
+         :map company-active-map
+         ("C-n" . company-select-next)
+         ("C-p" . company-select-previous)
+         ("C-d" . company-show-doc-buffer)
+         ("M-." . company-show-location))
+  :init
+  (add-hook 'c-mode-common-hook 'company-mode)
+  (add-hook 'sgml-mode-hook 'company-mode)
+  (add-hook 'emacs-lisp-mode-hook 'company-mode)
+  (add-hook 'text-mode-hook 'company-mode)
+  (add-hook 'lisp-mode-hook 'company-mode)
   :config
-  (global-company-mode 1)
-  :bind (:map company-active-map  
-            ("<tab>" . company-complete-selection))
-  :bind (:map company-search-map
-            ("C-n" . company-select-next)
-            ("C-p" . company-select-previous)))
+  (eval-after-load 'c-mode
+    '(define-key c-mode-map (kbd "[tab]") 'company-complete))
 
-;Run M-x company-tabnine-install-binary to install the TabNine binary for your system.
-(use-package company-tabnine
-  :config
-  (add-to-list 'company-backends #'company-tabnine)
-  (setq company-idle-delay 0.05)
+  (setq company-tooltip-limit 20)
   (setq company-show-numbers t)
-  (company-tng-configure-default)
-  (setq company-backends
-      '(
-        (company-tabnine :separate company-dabbrev company-keywords company-files company-ispell company-capf)
-        ))
-  (setq company-frontends
-        '(company-tng-frontend
-          company-pseudo-tooltip-frontend
-          company-echo-metadata-frontend)))
+  (setq company-dabbrev-downcase nil)
+  (setq company-idle-delay 0)
+  (setq company-echo-delay 0)
 
-(defvar company-mode/enable-yas t
-  "Enable yasnippet for all backends.")
+  (setq company-backends '(company-capf
+                           company-keywords
+                           company-semantic
+                           company-files
+                           company-etags
+                           company-elisp
+                           company-clang
+                           company-irony-c-headers
+                           company-irony
+                           company-jedi
+                           company-cmake
+                           company-ispell
+                           company-yasnippet))
 
-(defun company-mode/backend-with-yas (backend)
-  (if (or (not company-mode/enable-yas) (and (listp backend) (member 'company-yasnippet backend)))
-      backend
-    (append (if (consp backend) backend (list backend))
-            '(:with company-yasnippet))))
+	(defun my-company-visible-and-explicit-action-p ()
+    (and (company-tooltip-visible-p)
+         (company-explicit-action-p)))
+  (defun company-ac-setup ()
+    "Sets up `company-mode' to behave similarly to `auto-complete-mode'."
+    (setq company-require-match nil)
+    (setq company-auto-complete #'my-company-visible-and-explicit-action-p)
+    (setq company-frontends '(company-echo-metadata-frontend
+                              company-pseudo-tooltip-unless-just-one-frontend-with-delay
+                              company-preview-frontend))
+    (define-key company-active-map [tab]
+      'company-select-next-if-tooltip-visible-or-complete-selection)
+    (define-key company-active-map (kbd "TAB")
+      'company-select-next-if-tooltip-visible-or-complete-selection))
 
-(setq company-backends (mapcar #'company-mode/backend-with-yas company-backends))
+  (company-ac-setup)
+  (add-hook 'js2-mode-hook (lambda () (company-mode)))
+	
+  (global-company-mode))
 
-;; Add `company-elisp' backend for elisp.
-(add-hook 'emacs-lisp-mode-hook
-          #'(lambda ()
-              (require 'company-elisp)
-              (push 'company-elisp company-backends)))
+(use-package company-quickhelp
+  :after company
+  :config
+  (setq company-quickhelp-idle-delay 0.1)
+  (company-quickhelp-mode 1))
 
-;; Remove duplicate candidate.
-(add-to-list 'company-transformers #'delete-dups)
+;; (use-package company-irony
+;;   :ensure t
+;;   :after (company irony)
+;;   :commands (company-irony)
+;;   :config
+;;   (add-hook 'irony-mode-hook 'company-irony-setup-begin-commands))
+
+;; (use-package company-irony-c-headers
+;;   :ensure t
+;;   :commands (company-irony-c-headers)
+;;   :after company-irony)
+
+(use-package company-jedi
+  :ensure t
+  :commands (company-jedi)
+  :after (company python-mode))
+
+(use-package company-statistics
+  :ensure t
+  :after company
+  :config
+  (company-statistics-mode))
 
 (use-package php-mode
   :defer t
@@ -510,11 +566,11 @@
           ("\\.markdown\\'" . markdown-mode))
    :init (setq markdown-command "multimarkdown"))
 
-(use-package typescript-mode
-  :mode "\\.ts\\'"
-  :hook (typescript-mode . lsp-deferred)
-  :config
-  (setq typescript-indent-level 2))
+;; (use-package typescript-mode
+;;   :mode "\\.ts\\'"
+;;   :hook (typescript-mode . lsp-deferred)
+;;   :config
+;;   (setq typescript-indent-level 2))
 
 ;; Atalhos proprios
 
@@ -543,18 +599,6 @@
   ;;   ("C-e" move-end-of-line)
   ;;   ("C-v" scroll-down-command)
   ;;   ("M-v" scroll-up-command)
-  ;;   ("<begin>" beginning-of-buffer)(straight-use-package 'selectrum)
-;; (selectrum-mode +1)
-;; ;; to make sorting and filtering more intelligent
-;; (straight-use-package 'selectrum-prescient)
-;; (selectrum-prescient-mode +1)
-;; (prescient-persist-mode +1)
-
-;; (straight-use-package 'ivy)
-;; (ivy-mode 1)
-
-;; (setq ivy-use-selectable-prompt t)
-
   ;;   ("<C-end>" end-of-buffer)
   ;;   ("C-M-b" ergoemacs-backward-open-bracket)
   ;;   ("C-M-f" ergoemacs-backward-open-bracket)
@@ -589,11 +633,11 @@
 
 ;; (global-set-key (kbd "M-o") 'ace-window)
 
-(global-set-key (kbd "M-<tab>") 'transpose-frame)
+;; (global-set-key (kbd "M-<tab>") 'transpose-frame)
 
-;; ;; C-z to 'undo, the default is C-/.
-;; (global-unset-key "\C-z")
-;; (global-set-key "\C-z" 'undo)
+;; C-z to 'undo, the default is C-/.
+(global-unset-key "\C-z")
+(global-set-key "\C-z" 'undo)
 
 ;; ;; Enabling control-c and control-v copy and paste
 
@@ -607,3 +651,16 @@
 
 
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+	 '("e0628ee6c594bc7a29bedc5c57f0f56f28c5b5deaa1bc60fc8bd4bb4106ebfda" default)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(vertico-current ((t (:background "#3a3f5a")))))
